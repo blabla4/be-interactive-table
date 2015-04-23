a5b8c24ff012a982c1a33060dd2630a2b5a20c0d
Pouli <paul.deblicker@isen-lille.fr>
2015-04-23 15:27:33 +0200

    Add graph mother

--- public/main.js
+++ public/main.js
@@ -23,6 +23,7 @@ window.onload = function() {
     if(subMenuTemp !== null) {
       if (!subMenuTemp.is(e.target) && subMenuTemp.has(e.target).length === 0)
       {
+        $(".motherContainer").remove();
         setTimeout(function() {
           subMenuTemp.remove();
           subMenuTemp = null;
@@ -33,6 +34,7 @@ window.onload = function() {
     if(subMenuMonitor !== null) {
       if (!subMenuMonitor.is(e.target) && subMenuMonitor.has(e.target).length === 0)
       {
+        $(".icingaContainer").remove();
         setTimeout(function() {
           subMenuMonitor.remove();
           subMenuMonitor = null;
@@ -172,9 +174,21 @@ function createSubMenuTemp() {
 }
 
 function showTemp(id) {
-
+  switch(id) {
+    case 1:
+      divMother("known cranberry");
+      break;
+    case 2:
+      divMother("purple turon");
+      break;
+    case 3:
+      divMother("double passion");
+      break;
+    case 4:
+      divMother("double anooshaboor");
+      break;
+  }
 }
-
 
 function createSubMenuMonitor() {
   if(subMenuMonitor !== null) {
@@ -290,6 +304,277 @@ function getIcingaLog(callback) {
     callback(response);
   });
 }
+
+function divMother(cookie) {
+  var container = $('<div></div>', {
+    class: 'motherContainer draggable'
+  }).appendTo('#background');
+  $('<div></div>', {
+    class: 'graph',
+    id: cookie
+  }).appendTo(container);
+  getTemp(cookie, drawGraph);
+}
+
+function getTemp(cookie, callback) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				callback(cookie, JSON.parse(xmlhttp.responseText).data);
+			}
+		};
+		xmlhttp.open("GET", "http://10.134.15.1:4041/data/temperature/" + cookie, true);
+		xmlhttp.send();
+	};
+
+function drawGraph(cookie, dataTemp) {
+		console.log(cookie);
+    console.log(dataTemp);
+    console.log($("#" + cookie));
+		$("[ id = '" + cookie + "']").highcharts({
+			chart: {
+				type: 'spline'
+			},
+			title: {
+				text: 'Variation of temperature'
+			},
+			subtitle: {
+				text: 'Cookie: ' + cookie
+			},
+			xAxis: {
+				type: 'datetime',
+				dateTimeLabelFormats: {
+					month: '%e. %b. %Y',
+					year: '%b'
+				},
+				title: {
+					text: 'Date'
+				}
+			},
+			yAxis: {
+				title: {
+					text: 'Temperature (°C)'
+				},
+				min: 0
+			},
+			tooltip: {
+				headerFormat: '<b>{point.x:%e. %b. %Y}</b><br>',
+				pointFormat: '{point.y:.2f} °C'
+			},
+			plotOptions: {
+				spline: {
+					marker: {
+						enabled: true
+					}
+				}
+			},
+			legend: {
+				enabled: false
+			},
+			series: [{
+				name: 'Temperature',
+				data: (function() {
+						var value= [];
+						dataTemp.forEach(function(item) {
+							value.push({x: new Date(item.date), y: item.value});
+						});
+						return value;
+					}())
+			}]
+		});
+	};
+
+  Highcharts.theme = {
+  		 colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+  				"#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+  		 chart: {
+  				backgroundColor: {
+  					 linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+  					 stops: [
+  							[0, '#2a2a2b'],
+  							[1, '#3e3e40']
+  					 ]
+  				},
+  				style: {
+  					 fontFamily: "'Unica One', sans-serif"
+  				},
+  				plotBorderColor: '#606063'
+  		 },
+  		 title: {
+  				style: {
+  					 color: '#E0E0E3',
+  					 textTransform: 'uppercase',
+  					 fontSize: '20px'
+  				}
+  		 },
+  		 subtitle: {
+  				style: {
+  					 color: '#E0E0E3',
+  					 textTransform: 'uppercase'
+  				}
+  		 },
+  		 xAxis: {
+  				gridLineColor: '#707073',
+  				labels: {
+  					 style: {
+  							color: '#E0E0E3'
+  					 }
+  				},
+  				lineColor: '#707073',
+  				minorGridLineColor: '#505053',
+  				tickColor: '#707073',
+  				title: {
+  					 style: {
+  							color: '#A0A0A3'
+
+  					 }
+  				}
+  		 },
+  		 yAxis: {
+  				gridLineColor: '#707073',
+  				labels: {
+  					 style: {
+  							color: '#E0E0E3'
+  					 }
+  				},
+  				lineColor: '#707073',
+  				minorGridLineColor: '#505053',
+  				tickColor: '#707073',
+  				tickWidth: 1,
+  				title: {
+  					 style: {
+  							color: '#A0A0A3'
+  					 }
+  				}
+  		 },
+  		 tooltip: {
+  				backgroundColor: 'rgba(0, 0, 0, 0.85)',
+  				style: {
+  					 color: '#F0F0F0'
+  				}
+  		 },
+  		 plotOptions: {
+  				series: {
+  					 dataLabels: {
+  							color: '#B0B0B3'
+  					 },
+  					 marker: {
+  							lineColor: '#333'
+  					 }
+  				},
+  				boxplot: {
+  					 fillColor: '#505053'
+  				},
+  				candlestick: {
+  					 lineColor: 'white'
+  				},
+  				errorbar: {
+  					 color: 'white'
+  				}
+  		 },
+  		 legend: {
+  				itemStyle: {
+  					 color: '#E0E0E3'
+  				},
+  				itemHoverStyle: {
+  					 color: '#FFF'
+  				},
+  				itemHiddenStyle: {
+  					 color: '#606063'
+  				}
+  		 },
+  		 credits: {
+  				style: {
+  					 color: '#666'
+  				}
+  		 },
+  		 labels: {
+  				style: {
+  					 color: '#707073'
+  				}
+  		 },
+  		 drilldown: {
+  				activeAxisLabelStyle: {
+  					 color: '#F0F0F3'
+  				},
+  				activeDataLabelStyle: {
+  					 color: '#F0F0F3'
+  				}
+  		 },
+  		 navigation: {
+  				buttonOptions: {
+  					 symbolStroke: '#DDDDDD',
+  					 theme: {
+  							fill: '#505053'
+  					 }
+  				}
+  		 },
+  		 rangeSelector: {
+  				buttonTheme: {
+  					 fill: '#505053',
+  					 stroke: '#000000',
+  					 style: {
+  							color: '#CCC'
+  					 },
+  					 states: {
+  							hover: {
+  								 fill: '#707073',
+  								 stroke: '#000000',
+  								 style: {
+  										color: 'white'
+  								 }
+  							},
+  							select: {
+  								 fill: '#000003',
+  								 stroke: '#000000',
+  								 style: {
+  										color: 'white'
+  								 }
+  							}
+  					 }
+  				},
+  				inputBoxBorderColor: '#505053',
+  				inputStyle: {
+  					 backgroundColor: '#333',
+  					 color: 'silver'
+  				},
+  				labelStyle: {
+  					 color: 'silver'
+  				}
+  		 },
+  		 navigator: {
+  				handles: {
+  					 backgroundColor: '#666',
+  					 borderColor: '#AAA'
+  				},
+  				outlineColor: '#CCC',
+  				maskFill: 'rgba(255,255,255,0.1)',
+  				series: {
+  					 color: '#7798BF',
+  					 lineColor: '#A6C7ED'
+  				},
+  				xAxis: {
+  					 gridLineColor: '#505053'
+  				}
+  		 },
+  		 scrollbar: {
+  				barBackgroundColor: '#808083',
+  				barBorderColor: '#808083',
+  				buttonArrowColor: '#CCC',
+  				buttonBackgroundColor: '#606063',
+  				buttonBorderColor: '#606063',
+  				rifleColor: '#FFF',
+  				trackBackgroundColor: '#404043',
+  				trackBorderColor: '#404043'
+  		 },
+  		 legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+  		 background2: '#505053',
+  		 dataLabelsColor: '#B0B0B3',
+  		 textColor: '#C0C0C0',
+  		 contrastTextColor: '#F0F0F3',
+  		 maskColor: 'rgba(255,255,255,0.3)'
+  	};
+  	Highcharts.setOptions(Highcharts.theme);
 
 function createSubMenuTv() {
   if(subMenuTv !== null) {
